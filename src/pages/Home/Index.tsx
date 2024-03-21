acd746de61166374c298adf4c0962be8e81f2b21
JecianeSilva <jeciane_amorim@hotmail.com>
2024-03-20 22:09:37 -0300

    Update code of the blocks

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useRef } from 'react';
+import ReactModal from 'react-modal';
+
 import { Container, Content, InstructionListContainer } from './styles';
 import ToolBar from '../../components/Toolbar';
 
@@ -17,17 +19,50 @@ const Home: React.FC = () => {
       {
         "kind": "block",
         "type": "controls_whileUntil"
+      },
+      {
+        "kind": "block",
+        "type": "ligar_leds",
+      },
+      {
+        "kind": "block",
+        "type": "math_change"
       }
     ]
   };
+
+
 
   useEffect(() => {
     if (!blocklyDivRef.current) {
       return;
     }
 
-    // Blockly.common.defineBlocks(blocks);
+    var mathChangeJson = {
+      "message0": "Contar Pessoas %1",
+      "args0": [
+        { "type": "field_variable", "name": "VAR", "variable": "item", "variableTypes": [""] },
+      ],
+      "previousStatement": null,
+      "nextStatement": null,
+      "colour": 230
+    };
+    Blockly.Blocks['math_change'] = {
+      init: function () {
+        this.jsonInit(mathChangeJson);
+      }
+    };
 
+    Blockly.Blocks['ligar_leds'] = {
+      init: function () {
+        this.jsonInit({
+          message0: "Ligar leds",
+          "previousStatement": null,
+          "nextStatement": null,
+          "colour": 280
+        })
+      }
+    }
     // const workspace = Blockly.inject(blocklyDivRef.current, { toolbox: toolboxDevices, toolboxPosition: '', collapse: true });
     const workspace = Blockly.inject(blocklyDivRef.current, { toolbox: toolboxInstruction });
 
@@ -47,10 +82,17 @@ const Home: React.FC = () => {
               <img src="src/assets/icons/square.svg" alt="" height={22} width={22} />
               <span>Instruções</span>
             </label>
+            <button onClick={() => { }}>
+              <img src="src/assets/icons/square.svg" alt="" height={22} width={22} />
+            </button>
           </div>
         </InstructionListContainer>
         <div id="blocklyDiv" ref={blocklyDivRef} />
       </Content>
+      <ReactModal
+        open={open}
+      >
+      </ReactModal>
     </Container >
   );
 }
